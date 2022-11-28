c360ab6c0200fe35dafd3747075a9dbd515ae10a
Aare <aare212@hotmail.com>
2022-11-28 10:25:45 +0200

    Tehtävä 2.4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,13 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    summa = 0
+    ratings.each{|a| summa += a.score.to_f}
+    if ratings.count > 0
+      ka = summa / ratings.count
+      return "Beer has #{ratings.count} ratings with an average of #{ka}"
+    end
+  end
 end
