033cb760d688acf0fa8b25b69bb16c44db27abc3
Aare <aare212@hotmail.com>
2022-11-29 08:32:21 +0200

    Tehtävä 2.5 reduce, map sum, average

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,11 +3,16 @@ class Beer < ApplicationRecord
   has_many :ratings
 
   def average_rating
-    summa = 0
-    ratings.each{|a| summa += a.score.to_f}
+    #summa = 0
+    #ratings.each{|a| summa += a.score.to_f}     # .each
+    summa_map_sumilla = ratings.map{|a| a.score.to_f}.sum
+    #summa_reducella = ratings.map{|a| a.score.to_f}.reduce{|sum,a| sum+a}
+
+    ka_average = ratings.average(:score)
+
     if ratings.count > 0
-      ka = summa / ratings.count
-      return "Beer has #{ratings.count} ratings with an average of #{ka}"
+      #ka = summa_map_sumilla / ratings.count
+      return "Beer has #{ratings.count} ratings with an average of #{ka_average}"
     end
   end
 end
