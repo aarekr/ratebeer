772b07f60c02013f3bccc6989bb1f9caa7089dba
Aare <aare212@hotmail.com>
2022-12-01 08:05:23 +0200

    Tehtävä 2.14 panimon reittaukset

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,12 @@
 class Brewery < ApplicationRecord
   has_many :beers
+  has_many :ratings, through: :beers
+
+  def average_rating
+    ka_average = ratings.average(:score)
+
+    if ratings.count > 0
+      return "Brewey has #{ratings.count} #{"rating".pluralize(ratings.count)} with an average of #{ka_average}"
+    end
+  end
 end
