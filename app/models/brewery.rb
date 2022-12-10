894ca9b12af715635e4edec17bb9b493fca8763c
Aare <aare212@hotmail.com>
2022-12-10 08:26:05 +0200

    Tehtävä 3.8 year check

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,10 +1,21 @@
 class Brewery < ApplicationRecord
   include RatingAverage
 
+  def vuosi_check
+    vuosi_nyt = Time.now.year
+    puts "*** VUOSI NYT: #{vuosi_nyt} ***"
+    puts "*** YEAR: #{year} ***"
+    aika_nyt = Time.now
+    puts "*** AIKA NYT: #{aika_nyt} ***"
+    if year > vuosi_nyt
+      errors.add(:year, "can't be in the future")
+    end
+  end
+
   validates :name, length: { minimum: 1 }
-  validates :year, numericality: { greater_than_or_equal_to: 1040,
-                                   less_than_or_equal_to: 2022,
-                                   only_integer: true }
+  validates :year, numericality: { greater_than_or_equal_to: 1040 }
+  validate :vuosi_check
+  validates :year, numericality: { only_integer: true }
 
   has_many :beers
   has_many :ratings, through: :beers
