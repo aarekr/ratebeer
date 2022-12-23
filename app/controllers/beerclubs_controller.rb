082229a4bfa014f0d9f56348a9a4dd2a5aa04223
Aare <aare212@hotmail.com>
2022-12-23 08:11:48 +0200

    Tehtävä 6.3 Bootstrap creation forms

--- app/controllers/beerclubs_controller.rb
+++ app/controllers/beerclubs_controller.rb
@@ -37,7 +37,10 @@ class BeerclubsController < ApplicationController
 
   # POST /beerclubs or /beerclubs.json
   def create
+    puts "*** tultiin beerclub createen"
+    puts "*** tultiinn beerclub createen, beerclub_params: #{beerclub_params}"
     @beerclub = Beerclub.new(beerclub_params)
+    puts "*** create beerclub_params: #{beerclub_params}"
 
     respond_to do |format|
       if @beerclub.save
