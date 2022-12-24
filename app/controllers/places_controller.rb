cd91ef6a6ccd04ebb5872c7a5ca6cb32e9005823
Aare <aare212@hotmail.com>
2022-12-24 17:05:23 +0200

    Tehtävä 5.15 olutsää

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,10 +8,18 @@ class PlacesController < ApplicationController
   end
 
   def search
+    key = "..." # sää-key
+    url = "http://api.weatherstack.com/current"
+    response = HTTParty.get "#{url}?access_key=#{key}&query=#{ERB::Util.url_encode(params[:city])}"
+
+    @kaupunki = response["location"]["name"]
+    @lampotila = response["current"]["temperature"]
+    @saa_ikoni = response["current"]["weather_icons"]
+    @wind_speed = response["current"]["wind_speed"]
+    @wind_dir = response["current"]["wind_dir"]
+
     @places = BeermappingApi.places_in(params[:city])
     session[:vika_haku] = @places
-    puts "*** SEARCH session: #{session[:vika_haku]}"
-    puts "*** SEARCH session toka: #{session[:vika_haku].class}"
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -8,25 +8,22 @@ class BeermappingApi
   end
 
   def self.get_places_in(city)
-    # puts "*** tultiin get_places_in -funktioon"
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-    # puts "*** BeermappingApi response: #{response}"
+
     places = response.parsed_response["bmp_locations"]["location"]
-    # puts "*** BeermappingApi places: #{places}"
 
     return [] if places.is_a?(Hash) && places['id'].nil?
 
     places = [places] if places.is_a?(Hash)
     places.map do |place|
-      puts "*** BeermappingApi place: #{place}"
       Place.new(place)
     end
   end
 
   def self.key
-    "ddd910aa0b5ef3abdcca0736426a237d"
+    "..."
     # return nil if Rails.env.test? # testatessa ei apia tarvita, palautetaan nil
     # raise 'BEERMAPPING_APIKEY env variable not defined' if ENV['BEERMAPPING_APIKEY'].nil?
     # ENV.fetch('BEERMAPPING_APIKEY')
