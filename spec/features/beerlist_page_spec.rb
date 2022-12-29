de72b5b6937c2a39e2ea5e2e8f15a9aab2fe9cf0
Aare <aare212@hotmail.com>
2022-12-29 10:14:04 +0200

    Allow net connect

--- spec/features/beerlist_page_spec.rb
+++ spec/features/beerlist_page_spec.rb
@@ -8,8 +8,8 @@ describe "Beerlist page" do
         # options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
     end
     # Capybara.javascript_driver = :chrome
-    WebMock.disable_net_connect!(allow_localhost: true)
-    # WebMock.allow_net_connect!
+    # WebMock.disable_net_connect!(allow_localhost: true)
+    WebMock.allow_net_connect!
   end
 
   before :each do
